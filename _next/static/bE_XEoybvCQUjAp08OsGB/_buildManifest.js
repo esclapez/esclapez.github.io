4c2d3be6092243418c381fae5605a1dc04e2e2c8
Lucas Esclapez <lucas.esclapez@gmail.com>
2025-03-17 16:59:32 +0100

    Updates

---
+++ _next/static/bE_XEoybvCQUjAp08OsGB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/388-a4841478e6acbcb1.js","static/chunks/pages/index-95939f4c8cf92064.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/e_SDrWGWT07tYDYpWmgRJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/388-a4841478e6acbcb1.js","static/chunks/pages/index-bdfe5d42045e1476.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
