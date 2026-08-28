e8922495b7cadb9c0955ea8d19cb0754382aac3f
Lucas Esclapez <lucas.esclapez@gmail.com>
2026-08-28 19:45:50 +0200

    Updates

---
+++ _next/static/F4l0aVA8BESrYKLe_Uwe9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/786-80b45fe706648609.js","static/chunks/pages/index-c9bd5d3ae0afe988.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
